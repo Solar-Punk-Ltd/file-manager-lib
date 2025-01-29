2be7f54bef4e7a88e9a40cce5cd27db24c4d5b29
Peter Ott <peter.ott@solarpunk.buzz>
2025-01-29 12:14:24 +0100

    initFileInfoList unit tests pass

--- tests/fileManager.test.ts
+++ tests/fileManager.test.ts
@@ -1,8 +1,31 @@
 import { FileManager } from "../src/fileManager";
+import { returnDataTxt } from "./mockHelpers";
+import fs from 'fs';
 
-describe('test if works', () => {
-  it('should work', () => {
+
+describe('initFileInfoList', () => {
+  it('should load FileInfo list into memory', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(returnDataTxt);
+
     const fileManager = new FileManager();
-    console.log(fileManager.getFileInfoList());
-  });
+    
+    expect(fileManager.getFileInfoList()).toEqual([
+        {
+          "batchId": "ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51",
+          "eFileRef": "src/folder/1.txt"
+        },
+        {
+          "batchId": "ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51",
+          "eFileRef": "src/folder/2.txt"
+        }
+      ]);
+    });
+
+    it('should throw an error if fileInfoList is not an array', async () => {
+        (fs.readFileSync as jest.Mock).mockReturnValue(`{
+          "fileInfoList": "not an array"
+        }`);
+    
+        expect(() => new FileManager()).toThrowError("fileInfoList has to be an array!");
+    });
 });
