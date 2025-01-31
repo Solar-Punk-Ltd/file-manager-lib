3b00dc2db457857cb6bc062953f6dc1a9c6c95bb
Peter Ott <33352167+ottpeter@users.noreply.github.com>
2025-01-31 14:28:54 +0100

    Basic FileManager functionality, only local (#1)

--- src/fileManager.ts
+++ src/fileManager.ts
@@ -1,3 +1,7 @@
+import { BatchId } from '@ethersphere/bee-js';
+import fs from 'fs';
+
+import { FILE_INFO_PATH } from './constants';
 import { FileInfo, ShareItem } from './types';
 
 export class FileManager {
@@ -6,26 +10,102 @@ export class FileManager {
   constructor() {
     this.fileInfoList = [];
 
-    this.initialize();
+    // We said that constructor will load the file info list into state, but this only works as long as initialize is synchronous.
   }
 
-  async initialize(): Promise<void> {}
+  async initialize(): Promise<void> {
+    await this.initFileInfoList();
+  }
+
+  private async initFileInfoList(): Promise<void> {
+    const rawData = fs.readFileSync(FILE_INFO_PATH, 'utf8');
+    const data = JSON.parse(rawData);
+
+    if (!Array.isArray(data)) {
+      throw new TypeError('fileInfoList has to be an array!');
+    }
+
+    for (const fileInfo of data) {
+      this.fileInfoList.push(fileInfo);
+    }
+  }
 
   getFileInfoList(): FileInfo[] {
     return this.fileInfoList;
   }
 
   async saveFileInfo(fileInfo: FileInfo): Promise<string> {
-    return '';
+    try {
+      if (!fileInfo || !fileInfo.batchId || !fileInfo.eFileRef) {
+        throw new Error("Invalid fileInfo: 'batchId' and 'eFileRef' are required.");
+      }
+
+      const index = this.fileInfoList.length;
+      this.fileInfoList.push(fileInfo);
+
+      const data = JSON.stringify(this.fileInfoList);
+      fs.writeFileSync(FILE_INFO_PATH, data);
+
+      return index.toString(16).padStart(64, '0');
+    } catch (error) {
+      console.error('Error saving file info:', error);
+      throw error;
+    }
   }
 
-  listFiles(fileInfo: FileInfo): string[] {
-    return [];
+  // fileInfo might point to a folder, or a single file
+  // could name downloadFiles as well, possibly
+  // getDirectorStructure()
+  async listFiles(fileInfo: FileInfo): Promise<string> {
+    return fileInfo.eFileRef;
   }
 
-  async upload(filePath: string, customMetadata?: Record<string, string>): Promise<string> {
-    return '';
+  async upload(batchId: string | BatchId, filePath: string, customMetadata?: Record<string, string>): Promise<string> {
+    const fileInfo: FileInfo = {
+      eFileRef: filePath,
+      batchId: batchId,
+      customMetadata,
+    };
+
+    const ref = this.saveFileInfo(fileInfo);
+
+    return ref;
   }
 
-  async shareItems(items: ShareItem[], targetOverlays: string[], recipients: string[]): Promise<void> {}
+  async shareItems(items: ShareItem[], targetOverlays: string[], recipients: string[]): Promise<void> {
+    try {
+      for (let i = 0; i < items.length; i++) {
+        for (let j = 0; j < items[i].fileInfoList.length; j++) {
+          const ix = this.fileInfoList.findIndex((fileInfo) => fileInfo.eFileRef === items[i].fileInfoList[j].eFileRef);
+          if (ix === -1) {
+            throw new Error(`Could not find reference: ${items[i].fileInfoList[j].eFileRef}`);
+          }
+        }
+      }
+
+      for (const shareItem of items) {
+        this.sendShareMessage(targetOverlays, shareItem, recipients);
+      }
+    } catch (error) {
+      console.error('There was an error while trying to share items: ', error);
+      throw error;
+    }
+  }
+
+  async sendShareMessage(targetOverlays: string[], item: ShareItem, recipients: string[]): Promise<void> {
+    if (recipients.length === 0 || recipients.length !== targetOverlays.length) {
+      console.log('Invalid recipients or  targetoverlays specified for sharing.');
+      return;
+    }
+
+    for (let i = 0; i < recipients.length; i++) {
+      try {
+        const msgData = new Uint8Array(Buffer.from(JSON.stringify(item)));
+        console.log(`Sending message to ${recipients[i]}: `, msgData);
+        // Save this to a separate file, like data.txt. msgData should be saved into an array
+      } catch (error: any) {
+        console.log(`Failed to share item with recipient: ${recipients[i]}\n `, error);
+      }
+    }
+  }
 }
---
+++ tests/fileManager.test.ts
@@ -0,0 +1,235 @@
+import fs from 'fs';
+
+import { FileManager } from '../src/fileManager';
+
+import { emptyFileInfoTxt, extendedFileInfoTxt, fileInfoTxt, mockBatchId } from './mockHelpers';
+//import { ShareItem } from 'src/types';
+
+
+describe('initialize', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should load FileInfo list into memory', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+
+    expect(fileManager.getFileInfoList()).toEqual([
+      {
+        batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+        eFileRef: 'src/folder/1.txt',
+      },
+      {
+        batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+        eFileRef: 'src/folder/2.txt',
+      },
+    ]);
+  });
+
+  it('should throw an error if fileInfoList is not an array', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(`{
+      "fileInfoList": "not an array"
+    }`);
+
+    try {
+      const fileManager = new FileManager();
+      await fileManager.initialize();
+      fail('initialize should fail if fileInfo is not an array');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as Error).message).toBe('fileInfoList has to be an array!');
+    }
+  });
+});
+
+describe('saveFileInfo', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should save new FileInfo into data.txt', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    jest.spyOn(fs, 'writeFileSync').mockReturnValue();
+    const writeFileSync = jest.spyOn(fs, 'writeFileSync');
+
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+    const fileInfo = {
+      batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+      eFileRef: 'src/folder/3.txt',
+    };
+
+    const ref = await fileManager.saveFileInfo(fileInfo);
+
+    expect(ref).toBe('0000000000000000000000000000000000000000000000000000000000000002');
+    expect(writeFileSync).toHaveBeenCalledWith(expect.any(String), extendedFileInfoTxt);
+  });
+
+  it('should throw an error if fileInfo is invalid', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(emptyFileInfoTxt);
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+    const fileManagerSpy = jest.spyOn(fileManager, 'saveFileInfo');
+
+    const fileInfo = {
+      batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+    };
+
+    try {
+      await fileManager.saveFileInfo(fileInfo as any);
+      fail('Expected saveFileInfo to throw an error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as any).message).toBe("Invalid fileInfo: 'batchId' and 'eFileRef' are required.");
+      expect(fileManagerSpy).toHaveBeenCalledWith(fileInfo as any);
+    }
+  });
+
+  it('should throw an error if there is an error saving the file info', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    jest.spyOn(fs, 'writeFileSync').mockImplementation(() => {
+      throw new Error('Error saving file info');
+    });
+
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+    const fileInfo = {
+      batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+      eFileRef: 'src/folder/3.txt',
+    };
+
+    try {
+      await fileManager.saveFileInfo(fileInfo);
+      fail('Expected saveFileInfo to throw an error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as any).message).toBe('Error saving file info');
+    }
+  });
+});
+
+describe('listFiles', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should list paths (refs) for given input list', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+    const list = fileManager.getFileInfoList();
+
+    const path = await fileManager.listFiles(list[0]);
+
+    expect(path).toBe('src/folder/1.txt');
+  });
+});
+
+describe('upload', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should save FileInfo', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+
+    await fileManager.upload(mockBatchId, 'src/folder/3.txt');
+
+    expect(fileManager.getFileInfoList()).toHaveLength(3);
+    expect(fileManager.getFileInfoList()[2]).toEqual({
+      eFileRef: 'src/folder/3.txt',
+      batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+    });
+  });
+
+  it('should give back ref (currently index)', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+
+    const ref = await fileManager.upload(mockBatchId, 'src/folder/3.txt');
+
+    expect(ref).toBe('0000000000000000000000000000000000000000000000000000000000000002');
+  });
+
+  it('should work with consecutive uploads', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+
+    await fileManager.upload(mockBatchId, 'src/folder/3.txt');
+
+    expect(fileManager.getFileInfoList()).toHaveLength(3);
+    expect(fileManager.getFileInfoList()[2]).toEqual({
+      eFileRef: 'src/folder/3.txt',
+      batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+    });
+
+    await fileManager.upload(mockBatchId, 'src/folder/4.txt');
+
+    expect(fileManager.getFileInfoList()).toHaveLength(4);
+    expect(fileManager.getFileInfoList()[3]).toEqual({
+      eFileRef: 'src/folder/4.txt',
+      batchId: 'ee0fec26fdd55a1b8a777cc8c84277a1b16a7da318413fbd4cc4634dd93a2c51',
+    });
+  });
+});
+
+/*
+describe('shareItems', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should call sendShareMessage', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(dataTxt);
+    const sendShareMessageSpy = jest.spyOn(FileManager as any, 'sendShareMessage');
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+
+    const items: ShareItem[] = [
+      {
+        fileInfoList: fileManager.getFileInfoList(),
+        message: "Dear Friend! I'm sharing these files with you.",
+        timestamp: 100
+      }
+    ];
+
+    const targetOverlays = [ "friendsOverlay" ];
+    const addresses = [ "friendsAddress" ];
+
+    fileManager.shareItems(items, targetOverlays, addresses);
+
+    expect(sendShareMessageSpy).toHaveBeenCalledTimes(1);
+  });
+});
+*/
+
+describe('upload and listFiles', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should give back correct refs by listFiles, after upload', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(fileInfoTxt);
+    const fileManager = new FileManager();
+    await fileManager.initialize();
+
+    let list = fileManager.getFileInfoList();
+    let path = await fileManager.listFiles(list[0]);
+
+    expect(path).toBe('src/folder/1.txt');
+
+    await fileManager.upload(mockBatchId, 'src/folder/3.txt');
+
+    list = fileManager.getFileInfoList();
+    path = await fileManager.listFiles(list[2]);
+
+    expect(path).toBe('src/folder/3.txt');
+  });
+});
